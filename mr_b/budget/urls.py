cf6a524ca08d77d8d90e037247e57badd87dbb92
ManiSai18 <manisss180@gmail.com>
2025-06-13 18:48:19 -0500

    Fresh migration reset done

--- mr_b/budget/urls.py
+++ mr_b/budget/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard'),
+    path('', views.index, name='index'),
 
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
